50fedeb674ebd50e50ddab9b562feabcd965dc30
Student User <studentuser@d-10-18-252-56.dhcp4.washington.edu>
2019-02-14 19:10:53 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Student User on 2/14/19.
+//  Copyright © 2019 University of Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
