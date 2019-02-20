ad85fb00e14fe5f1b2481ba7a734f2e9151c042c
Student User <studentuser@D-10-19-213-4.dhcp4.washington.edu>
2019-02-19 17:17:42 -0800

    Started making progress. Settingogress. Created the TableView, Toolbar, and first cell. Created a new quiz file (for later). Created QuizCell.

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let quizzes: [String] = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
 }
+/*
+protocol quizRepository {
+    func getNumber() -> UInt
+    func getQuestions -> [String]
+    func saveAnswers
+}
+*/
 
+class QuizCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    var icon: UIImage! = nil //Image to represent quiz
+    var subject: String = ""; //Title 30 char limit
+    var desc: String = ""; //Description; Short Sentence
+    
+    
+    
+    
+}
---
+++ iQuiz/quiz.swift
@@ -0,0 +1,12 @@
+//
+//  quiz.swift
+//  iQuiz
+//
+//  Created by Student User on 2/19/19.
+//  Copyright © 2019 University of Washington. All rights reserved.
+//
+
+import UIKit
+
+class quiz {
+}
