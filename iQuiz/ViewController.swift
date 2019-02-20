eb066592851505ed1402406484745cc1dcbe7606
Student User <studentuser@D-10-19-213-4.dhcp4.washington.edu>
2019-02-19 19:13:53 -0800

    Completed part 1.

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -11,20 +11,46 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
-    let quizzes: [String] = ["Mathematics", "Marvel Super Heroes", "Science"]
+    let titles: [String] = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    let descriptions: [String] = ["Test your math skills!", "Woh, Superheroes!", "What's an atom?"]
+    let icons: [String] = ["images/math_ico.jpg", "images/marvel_ico.png", "images/science_ico.jpg"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return quizzes.count
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "qcell", for: indexPath) as! QuizCell
+        let str = titles[indexPath.item]
+        
+        //Takes first 30 characters of a string
+        //Because substrings are so unnecessarily complicated.
+        if(str.count > 30) {
+            let start = str.index(str.startIndex, offsetBy: 0)
+            let end = str.index(str.endIndex, offsetBy: 30  )
+            let range = start..<end
+            cell.title.text = String(str[range])
+        } else {
+            cell.title.text = str
+        }
+        cell.desc.text = descriptions[indexPath.item]
+        cell.imageView?.image = UIImage(named: icons[indexPath.item])
+        print("Cell Initialized")
+        return cell
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func settings(_ btn: UIBarButtonItem!) {
+        let alert = UIAlertController(title: "Settings", message: "go here.", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
@@ -37,10 +63,9 @@ protocol quizRepository {
 */
 
 class QuizCell: UITableViewCell {
-    @IBOutlet weak var title: UILabel!
-    var icon: UIImage! = nil //Image to represent quiz
-    var subject: String = ""; //Title 30 char limit
-    var desc: String = ""; //Description; Short Sentence
+    @IBOutlet weak var title: UILabel! //30 char limit
+    @IBOutlet weak var desc: UILabel! //Description; Short Sentence
+    @IBOutlet weak var icon: UIImageView! //Image to represent quiz
     
     
     
