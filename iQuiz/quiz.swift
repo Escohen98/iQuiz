1e590f48f2dfd8a13a8556007673bde29127c64c
Student User <studentuser@D-10-18-206-67.dhcp4.washington.edu>
2019-02-21 19:05:53 -0800

    Finished making Questions storyboard CollectionView. Made progress on Quiz class.

--- iQuiz/quiz.swift
+++ iQuiz/quiz.swift
@@ -9,4 +9,75 @@
 import UIKit
 
 class quiz {
+    
+    /*
+    * Initializes a quiz object.
+    * @param Subject (String) - Quiz Topic
+    * @param questions (NSDictionary<String, Array<String>) - A dictionary containing all of the questions
+    * and an array of the answers for each question.
+    */
+    init(subject: String, questions: NSDictionary) {
+        self.subject = subject
+        self.questions = questions
+        QCOUNT = questions.count
+        resetAnswers()
+    }
+    
+    private let QCOUNT : Int //Number of questions.
+    private var subject : String = ""
+    private var questions : NSDictionary //Contains all questions/answer pairs (String/Array<String>)
+    private var scores : Array<Bool> = Array<Bool>() //Contains booleans of whether a user got the question correct or not.
+    
+    //Returns the subject
+    func getSubject() -> String {
+        return subject
+    }
+    
+    //Returns the number of questions.
+    func getQuestionCount() -> Int {
+        return QCOUNT
+    }
+    
+    /*
+     * Returns an array with 1 question if the index is provided and is within range.
+     * Returns an empty array if index is not in range
+     * Returns an array of all of the questions.
+     */
+    func getQuestion(index: Int = -1) -> [Any] {
+        let keys = questions.allKeys
+        if(index == -1) {
+            return keys
+        } else if index > QCOUNT-1 || index < 0 {
+            return []
+        }
+        return [keys[index]]
+    }
+    
+    /*
+     * Takes in a key string and returns the corresponding value pair if exists.
+     * Otherwise returns an empty array.
+     */
+    func getAnswers(key: String) -> Any {
+        if((questions[key]) != nil) {
+            return questions[key]!
+        } else {
+            return []
+        }
+    }
+    
+    func setCorrect(index: Int) -> Bool {
+        if(index >= 0 && index < QCOUNT) {
+            scores[index] = true
+            return scores[index]
+        }
+        return false
+    }
+    
+    //Sets all scores array to array of false of size QCOUNT.
+    func resetAnswers() {
+        for _ in 1...QCOUNT {
+            scores.append(false)
+        }
+    }
+    
 }
